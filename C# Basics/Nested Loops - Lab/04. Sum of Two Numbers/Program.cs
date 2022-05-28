9a827d024aa6eb57bf1d89f758b2ecf2e82ef1a0
Peter Ivanov <deamon1888@gmail.com>
2022-05-28 03:12:13 +0300

    Lab6 Push

---
+++ C# Basics/Nested Loops - Lab/04. Sum of Two Numbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Dynamic;
+
+namespace _04._Sum_of_Two_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+            int magic = int.Parse(Console.ReadLine());
+            int counter = 0;
+            bool isMagic = false;
+            for (int i = start; i <= end; i++)
+            {
+                for (int j = start; j <= end; j++)
+                {
+                    counter++;
+                    if (i + j == magic)
+                    {
+                        isMagic = true;
+                        Console.WriteLine($"Combination N:{counter} ({i} + {j} = {magic})");
+                        return;
+                    }
+                }
+            }
+
+            if (!isMagic)
+            {
+                Console.WriteLine($"{counter} combinations - neither equals {magic}");
+            }
+        }
+    }
+}
---
+++ C# Basics/Nested Loops - Lab/05. Travelling/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _05._Travelling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string destination = Console.ReadLine();
+            while (destination != "End")
+            {
+                double savedMoney = 0;
+                double moneyNeeded = double.Parse(Console.ReadLine());
+                while (savedMoney < moneyNeeded)
+                {
+                    double money = double.Parse(Console.ReadLine());
+                    savedMoney += money;
+                }
+
+                
+                Console.WriteLine($"Going to {destination}!");
+                destination = Console.ReadLine();
+            }
+        }
+    }
+}
